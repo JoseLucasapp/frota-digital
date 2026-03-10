7bdeb5a8a43dea8fb335e0bc4524228aebad2ac8
joselucasapp <jlgf.profissional@gmail.com>
2026-03-10 02:04:50 -0300

    feat: create fuelings routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,13 @@
 const adminRoutes = require("./admin.routes")
 const authRoutes = require("./auth.routes")
 const driverRoutes = require("./driver.routes")
+const fuelingsRoutes = require("./fuelings.routes")
 const mechanicRoutes = require("./mechanic.routes")
 
-module.exports = (router) =>{
+module.exports = (router) => {
     authRoutes(router)
     adminRoutes(router)
     mechanicRoutes(router)
     driverRoutes(router)
+    fuelingsRoutes(router)
 }
